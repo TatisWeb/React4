d2465661ee4193dac0a055e2f2912f8f3bb049a7
Sergey Tarsenko <67330524+TatisWeb@users.noreply.github.com>
2020-11-25 21:28:45 +0400

    Update profile-reducer.js

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,28 +1,21 @@
-const profileReducer = (state,action) => {
-
-
 const UPDATE_NEW_POST = 'UPDATE_NEW_POST';
-const ADD_POST = 'ADD_POST';   
-      
+const ADD_POST = 'ADD_POST';
+
+const profileReducer = (state,action) => {
       
       if (action.type === 'ADD_POST') {
         
       let text = 
-this._state.profilePage.newPostText;
-this._state.profilePage.newPostText = '';   this._state.profilePage.posts.push({id:9,message: text , likeCount: 9});
-this._callSubscriber(this._state);
+          state.profilePage.newPostText;
+          state.profilePage.newPostText = '';   state.profilePage.posts.push({id:9,message: text , likeCount: 9});
       }
       
       else if 
-        (action.type === 'UPDATE_NEW_POST')        {    
-          this._state.profilePage.newPostText = action.newText;
- this._callSubscriber(this._state);          
-         
+         (action.type === 'UPDATE_NEW_POST')        {    
+          state.profilePage.newPostText = action.newText;
           }
       
-            
-  
   return state;
 }
 
-
+export default profilePage;
