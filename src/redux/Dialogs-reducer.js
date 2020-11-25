bc5b735834348ae6c83fd5e8924bc4d12e728748
Sergey Tarsenko <67330524+TatisWeb@users.noreply.github.com>
2020-11-25 21:38:54 +0400

    Update Dialogs-reducer.js

--- src/redux/Dialogs-reducer.js
+++ src/redux/Dialogs-reducer.js
@@ -4,14 +4,13 @@ const SEND_MESSAGE = 'SEND_MESSAGE';
 
 const dialogReducer = (state, action ) => {
 if (action.type === 'UPDATE_NEW_MESSAGE_BODY'){
-        this._state.dialogsPage.newMessageBody = action.body;
- this._callSubscriber(this._state);
+        state.dialogsPage.newMessageBody = action.body;
+ 
             } 
       else if (action.type === 'SEND_MESSAGE'){
-        let body = this._state.dialogsPage.newMessageBody;
-this._state.dialogsPage.newMessageBody = '';
-this._state.dialogsPage.messages.push({id: 7, message: body });
-this._callSubscriber(this._state);
+        let body = state.dialogsPage.newMessageBody;
+state.dialogsPage.newMessageBody = '';
+state.dialogsPage.messages.push({id: 7, message: body })
       }
               }
 return state;
